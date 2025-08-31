9e39333fbad638e2d19f020d5a1758aae91dca94
The-cool-loser <S8EdgyMusic@gmail.com>
2025-08-31 13:31:08 +0300

    Added 2nd article

--- Program.js
+++ Program.js
@@ -1,2 +1,13 @@
 var TalMosseri = "I'm Tal Mosseri";
 TalMosseri += " & YOU'RE NOT";
+var j = document.getElementsByClassName("navbar");
+/*
+var site = prompt("Insert a link", "youtube.com");
+var linkers = document.getElementById("website");
+if(site.substr(0,4) != "http"){
+    site = "http://" + site;
+    linkers.innerHTML = "<a href= '" +site + "'>" + site + "</a>";
+} else{
+    linkers.innerHTML = "<a href= '" +site + "'>" + site + "</a>";
+}
+*/
